435781e34f2baac4456d0602de210b0410f9c277
snake <824475386@qq.com>
2020-04-21 14:48:31 +0800

    first commit

---
+++ spring-boot-project/snake-spring-boot/src/main/java/com/snake/SnakeApplication.java
@@ -0,0 +1,15 @@
+package com.snake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author snake
+ * @date 2020/4/21 12:23.
+ */
+@SpringBootApplication
+public class SnakeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeApplication.class, args);
+	}
+}
---
+++ spring-boot-project/snake-spring-boot/src/main/java/com/snake/SnakeController.java
@@ -0,0 +1,8 @@
+package com.snake;
+
+/**
+ * @author snake
+ * @date 2020/4/21 12:29.
+ */
+public class SnakeController {
+}
