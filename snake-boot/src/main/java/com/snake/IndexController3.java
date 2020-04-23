388b8784ef8c0bbcc8f378725109402813fd873a
snake <824475386@qq.com>
2020-04-23 15:46:42 +0800

    测试 spring boot handlerMapping

---
+++ snake-boot/src/main/java/com/snake/IndexController3.java
@@ -0,0 +1,21 @@
+package com.snake;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author snake
+ * @date 2020/4/22 00:14
+ */
+@Component("/index3")
+public class IndexController3 implements HttpRequestHandler {
+
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("-----index3------");
+	}
+}
